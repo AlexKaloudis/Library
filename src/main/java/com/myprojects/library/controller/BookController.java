8b67e8859950911b4520853c9fd5a1b35b516844
Alexandros Kaloudis <a.kaloudis@msensis.com>
2023-11-26 21:44:00 +0200

    GetBook and DeleteBook were added.

--- src/main/java/com/myprojects/library/controller/BookController.java
+++ src/main/java/com/myprojects/library/controller/BookController.java
@@ -1,11 +1,15 @@
 package com.myprojects.library.controller;
 
 import com.myprojects.library.model.Book;
+import com.myprojects.library.model.Library;
 import com.myprojects.library.service.BookService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("api/v1/books")
@@ -21,9 +25,23 @@ public class BookController {
         return bookService.getAllBooks();
     }
 
+    @GetMapping("{id}")
+    public Book GetBook(@PathVariable String id) {
+        return bookService.GetBook(id);
+    }
+
+    @PutMapping
+    public ResponseEntity<Book> UpdateBook(@RequestBody Book book) {
+        return ok(bookService.UpdateBook(book));
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void AddMultipleBooks(@RequestBody List<Book> books) {
         bookService.addListOfBooks(books);
+    }
+    @DeleteMapping("{bookId}")
+    public void DeleteBook(@PathVariable String bookId) {
+        bookService.deleteBook(bookId);
     }
 }
